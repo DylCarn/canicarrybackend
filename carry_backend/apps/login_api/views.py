6a2b79e3caf5b9489345a46e4d0a7a0e010cd0e6
DylCarn <232230723+DylCarn@users.noreply.github.com>
2024-02-26 20:10:57 -0600

    first commit

---
+++ carry_backend/apps/login_api/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import Group, User
+from apps.login_api.serializers import UserSerializer
+from rest_framework import generics
+class UserViewSet(generics.ListAPIView):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    #permission_classes = [permissions.IsAuthenticated]
