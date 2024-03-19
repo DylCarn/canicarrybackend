0dac7258aa51a6889212292ab0aa4c7c7c6ed5e7
AlexProv1987 <alex.provenzano87@gmail.com>
2024-03-18 20:06:44 -0500

    Added Functionality for Auth users to "sign" Disclaimer

--- carry_backend/apps/disclaimer_api/urls.py
+++ carry_backend/apps/disclaimer_api/urls.py
@@ -2,4 +2,5 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
     path("last_disclaimer/", views.ReturnLastDisclaimer.as_view()),
+    path("sign_disclaimer/", views.SignDisclaimer.as_view()),
 ]
--- carry_backend/apps/disclaimer_api/views.py
+++ carry_backend/apps/disclaimer_api/views.py
@@ -1,16 +1,41 @@
-from django.shortcuts import render
-from rest_framework import generics
 from .serializers import DisclaimerSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .models import Disclamer
-# Create your views here.
+from apps.user_api.models import CarryUser
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
+def GetLastDisclaimer():
+    disclaimer = Disclamer.objects.last()
+    return disclaimer
+
 class ReturnLastDisclaimer(APIView):
     def get(self, request, format=None):
-        disclaimer = Disclamer.objects.last()
-        serializer = DisclaimerSerializer(disclaimer, many=False)
+        serializer = DisclaimerSerializer(GetLastDisclaimer(), many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
 class ReturnAllDisclaimers():
     pass
+
+class SignDisclaimer(APIView):
+    permission_classes=(IsAuthenticated,)
+
+    def post(self,request):
+        if self._sign_disclaimer(request):
+            return Response(data={'is_signed':True}, status=status.HTTP_200_OK)
+        else:
+            return Response(data={'is_signed':False}, status=status.HTTP_418_IM_A_TEAPOT)
+    
+    def _sign_disclaimer(self, request):
+        current_disclaimer = GetLastDisclaimer()
+        if current_disclaimer.pk == int(request.data['id']):
+            try:
+                user_obj = CarryUser.objects.get(pk=request.user.pk)
+                user_obj.disclaimer_id=current_disclaimer
+                user_obj.date_disclaimer_signed=datetime.now()
+                user_obj.save()
+                return True
+            except:
+                return False
+        return False
